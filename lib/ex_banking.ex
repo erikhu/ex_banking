7a68c4678cf34f389950362cb963eb4da64d8062
Erik Gonzalez <erikhuboy@gmail.com>
2022-02-16 22:41:15 -0500

    create user and validate arguments

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -12,12 +12,18 @@ defmodule ExBanking do
       :world
 
   """
+
+  alias ExBanking.User
+
   @spec deposit(user :: String.t, amount :: number, currency :: String.t) :: {:ok, new_balance :: number} | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
   def deposit(user, amount, currency) do
   end
 
   @spec create_user(user :: String.t) :: :ok | {:error, :wrong_arguments | :user_already_exists}
   def create_user(user) do
+    with :ok <- validate_arguments([is_bitstring(user)]) do
+      User.create_user(user)
+    end
   end
 
   @spec withdraw(user :: String.t, amount :: number, currency :: String.t) :: {:ok, new_balance :: number} | {:error, :wrong_arguments | :user_does_not_exist | :not_enough_money | :too_many_requests_to_user}
@@ -30,5 +36,13 @@ defmodule ExBanking do
 
   @spec send(from_user :: String.t, to_user :: String.t, amount :: number, currency :: String.t) :: {:ok, from_user_balance :: number, to_user_balance :: number} | {:error, :wrong_arguments | :not_enough_money | :sender_does_not_exist | :receiver_does_not_exist | :too_many_requests_to_sender | :too_many_requests_to_receiver}
   def send(from_user, to_user, amount, currency) do
+  end
+
+  defp validate_arguments(arguments) do
+    if Enum.find_value(arguments, false, fn arg -> not arg end) do
+      {:error, :wrong_arguments}
+    else
+      :ok
+    end
   end
 end
