c6e51e0d572986a74008f6da8a7a2b140184d614
Erik Gonzalez <erikhuboy@gmail.com>
2022-02-17 00:10:52 -0500

    user genserver create user with dynamicsupervisor

--- lib/ex_banking/user.ex
+++ lib/ex_banking/user.ex
@@ -1,9 +1,26 @@
 defmodule ExBanking.User do
-  @spec create_user(user :: String.t) :: :ok | {:error, :wrong_arguments | :user_already_exists}
-  def create_user(user) do
+  use GenServer
+
+  alias __MODULE__
+
+  @impl true
+  def start_link(user) do
+    name = String.to_atom(user)
+    GenServer.start_link(__MODULE__, :ok, name: name)
   end
 
-  @spec get_user(user :: String.t) :: {:ok, resource} | {:error, :user_does_not_exist}
-  def get_user(user) do
+  @impl true
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  @spec create_user(user :: String.t) :: :ok | {:error, :user_already_exists}
+  def create_user(user) do
+    case DynamicSupervisor.start_child(ExBanking.DynamicSupervisor, %{id: __MODULE__, start: {User, :start_link, [user]}})  do
+      {:ok, _} ->
+        :ok
+      _ ->
+        {:error, :user_already_exists}
+    end
   end
 end
