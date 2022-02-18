1e6f5b002eedf36ed18506b3faef503d9c450b42
Erik Gonzalez <erikhuboy@gmail.com>
2022-02-17 22:04:33 -0500

    add new line in ex_banking spec

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -5,7 +5,8 @@ defmodule ExBanking do
 
   alias ExBanking.User
 
-  @spec deposit(user :: String.t, amount :: number, currency :: String.t) :: {:ok, new_balance :: number} | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  @spec deposit(user :: String.t, amount :: number, currency :: String.t) ::
+  {:ok, new_balance :: number} | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
   def deposit(user, amount, currency) do
     with :ok <- validate_arguments([is_bitstring(user), is_number(amount), is_bitstring(currency), amount >= 0]),
          {:ok, pid} <- User.get_user(user) do
@@ -20,15 +21,18 @@ defmodule ExBanking do
     end
   end
 
-  @spec withdraw(user :: String.t, amount :: number, currency :: String.t) :: {:ok, new_balance :: number} | {:error, :wrong_arguments | :user_does_not_exist | :not_enough_money | :too_many_requests_to_user}
+  @spec withdraw(user :: String.t, amount :: number, currency :: String.t) ::
+  {:ok, new_balance :: number} | {:error, :wrong_arguments | :user_does_not_exist | :not_enough_money | :too_many_requests_to_user}
   def withdraw(user, amount, currency) do
   end
 
-  @spec get_balance(user :: String.t, currency :: String.t) :: {:ok, balance :: number} | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  @spec get_balance(user :: String.t, currency :: String.t) ::
+  {:ok, balance :: number} | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
   def get_balance(user, currency) do
   end
 
-  @spec send(from_user :: String.t, to_user :: String.t, amount :: number, currency :: String.t) :: {:ok, from_user_balance :: number, to_user_balance :: number} | {:error, :wrong_arguments | :not_enough_money | :sender_does_not_exist | :receiver_does_not_exist | :too_many_requests_to_sender | :too_many_requests_to_receiver}
+  @spec send(from_user :: String.t, to_user :: String.t, amount :: number, currency :: String.t) ::
+  {:ok, from_user_balance :: number, to_user_balance :: number} | {:error, :wrong_arguments | :not_enough_money | :sender_does_not_exist | :receiver_does_not_exist | :too_many_requests_to_sender | :too_many_requests_to_receiver}
   def send(from_user, to_user, amount, currency) do
   end
 
